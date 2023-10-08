f093236d7c7702ce3f37f6580f29f6cdbbe332c7
Jessica Sousa <jessica.persou@gmail.com>
2023-10-08 17:19:41 -0300

    project junit

---
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -0,0 +1,27 @@
+package com.algaworks.junit.utilidade;
+
+import java.math.BigDecimal;
+
+public class ContaBancaria {
+
+    public ContaBancaria(BigDecimal saldo) {
+        //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
+        //TODO 2 - pode ser zero ou negativo
+    }
+
+    public void saque(BigDecimal valor) {
+        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        //TODO 2 - Deve subtrair o valor do saldo
+        //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+    }
+
+    public void deposito(BigDecimal valor) {
+        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        //TODO 2 - Deve adicionar o valor ao saldo
+    }
+
+    public BigDecimal saldo() {
+        //TODO 1 - retornar saldo
+        return null;
+    }
+}
